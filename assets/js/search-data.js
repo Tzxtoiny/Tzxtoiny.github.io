4a85f65a398e65a7f6aed5cfad990e2b73d9c7f3
Tzxtoiny <62111393+Tzxtoiny@users.noreply.github.com>
2025-11-21 15:36:29 +0000

    Deploying to gh-pages from @ Tzxtoiny/Tzxtoiny.github.io@1091d449c1428904f9f42bf95d03057844958bb0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-study-on-the-human-centaur-system-for-load-carriage-assistance-has-been-accepted-for-publication-in-the-international-journal-of-robotics-research-ijrr",
+          title: 'Our study on the Human–Centaur System for Load-Carriage Assistance has been accepted for...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
